8a76ffc56044ffeb9b0d5ee4e2440b0f09d20507
velikhanov <teymurvk@mail.ru>
2021-04-24 00:44:55 +0400

    Update

--- app/Providers/GoogleDriveServiceProvider.php
+++ app/Providers/GoogleDriveServiceProvider.php
@@ -21,6 +21,9 @@ class GoogleDriveServiceProvider extends ServiceProvider
             $client = new Google_Client();
             $client->setClientId($config['clientId']);
             $client->setClientSecret($config['clientSecret']);
+            $client->setRedirectUri($this->_redirectURI);
+            $client->setAccessType('offline');
+            $client->setApprovalPrompt('force');
             $client->refreshToken($config['refreshToken']);
             $service = new \Google_Service_Drive($client);
             $adapter = new GoogleDriveAdapter($service, $config['folderId']);
