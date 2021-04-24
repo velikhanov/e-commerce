17e536585b52783dcc87ab62e29fb6bb62210b28
velikhanov <teymurvk@mail.ru>
2021-04-24 05:16:14 +0400

    Update

--- config/filesystems.php
+++ config/filesystems.php
@@ -58,10 +58,10 @@ return [
             // ...
         'google' => [
             'driver' => 'google',
-            'clientId' => env('MAIN_GOOGLE_DRIVE_CLIENT_ID'),
-            'clientSecret' => env('MAIN_GOOGLE_DRIVE_CLIENT_SECRET'),
-            'refreshToken' => env('MAIN_GOOGLE_DRIVE_REFRESH_TOKEN'),
-            'folderId' => env('MAIN_GOOGLE_DRIVE_FOLDER_ID'),
+            'clientId' => env('GOOGLE_DRIVE_CLIENT_ID'),
+            'clientSecret' => env('GOOGLE_DRIVE_CLIENT_SECRET'),
+            'refreshToken' => env('GOOGLE_DRIVE_REFRESH_TOKEN'),
+            'folderId' => env('GOOGLE_DRIVE_FOLDER_ID'),
         ],
         // ...
 
