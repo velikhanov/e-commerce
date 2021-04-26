5370ea4b6da56be2aefaf4afb75c4cac205fbafb
velikhanov <teymurvk@mail.ru>
2021-04-26 15:26:29 +0400

    Update

--- app/Http/ViewComposers/UsersPersonalDaraComposer.php
+++ app/Http/ViewComposers/UsersPersonalDaraComposer.php
@@ -19,7 +19,7 @@ class UsersPersonalDaraComposer
       ->where('filename', '=', pathinfo($user->img, PATHINFO_FILENAME))
       ->where('extension', '=', pathinfo($user->img, PATHINFO_EXTENSION))
       ->first();
-    $userimg = isset($file['path'])?(Storage::disk('google')->exists($file['path'])?Storage::disk('google')->get($file['path']):NULL):NULL
+    $userimg = isset($file['path'])?(Storage::disk('google')->exists($file['path'])?Storage::disk('google')->get($file['path']):NULL):NULL;
     };
 
     return $view->with('userimg', $userimg);
