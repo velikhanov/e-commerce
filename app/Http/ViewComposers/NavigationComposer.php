a7e5a0aabeacb6df92b13fcdb01fc89daad09bf8
velikhanov <teymurvk@mail.ru>
2021-04-26 19:40:02 +0400

    Update

--- app/Http/ViewComposers/NavigationComposer.php
+++ app/Http/ViewComposers/NavigationComposer.php
@@ -26,7 +26,18 @@ class NavigationComposer
         $catimg = isset($cat->img)?(isset($file['path'])?(Storage::disk('google')->exists($file['path'])?Storage::disk('google')->url($file['path']):NULL):NULL):NULL;
       };
       $subcatimg = Category::with('children')->where('parent_id', '<>', NULL)->get();
-
+      foreach ($subcatimg as $subcat) {
+        if(isset($subcat->img)){
+            // $cat = Category::
+            // code...
+            $contents = collect(Storage::disk('google')->listContents('1lngtMrfEvcwjnJWp6b7Bxv2q5NDdYJze/', false));
+            $file = $contents
+            ->where('type', '=', 'file')
+            ->where('filename', '=', pathinfo($subcat->img, PATHINFO_FILENAME))
+            ->where('extension', '=', pathinfo($subcat->img, PATHINFO_EXTENSION))
+            ->first();
+        };
+      }
       return $view->with(['catalog' => $catalog, 'catimg' => $catimg, 'subcatimg' => $subcatimg]);
   }
 }
