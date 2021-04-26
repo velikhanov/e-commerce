3a922a95cfabd16493732e6bed22877640d6d49b
velikhanov <teymurvk@mail.ru>
2021-04-26 19:21:05 +0400

    Update

--- app/Http/ViewComposers/NavigationComposer.php
+++ app/Http/ViewComposers/NavigationComposer.php
@@ -4,6 +4,7 @@ namespace App\Http\ViewComposers;
 
 use Illuminate\View\View;
 use App\Models\Category;
+use Illuminate\Support\Facades\Storage;
 
 class NavigationComposer
 {
@@ -11,20 +12,17 @@ class NavigationComposer
   {
       $catalog = Category::with('children')->where('parent_id', '=', NULL)->get();
       //
-      $categories = Category::get();
-      if(isset($categories->img)){
+      if(isset($catalog->img)){
           // $cat = Category::
-        foreach ($categories as $cat) {
           // code...
           $contents = collect(Storage::disk('google')->listContents('1lngtMrfEvcwjnJWp6b7Bxv2q5NDdYJze/', false));
           $file = $contents
           ->where('type', '=', 'file')
-          ->where('filename', '=', pathinfo($cat->img, PATHINFO_FILENAME))
-          ->where('extension', '=', pathinfo($cat->img, PATHINFO_EXTENSION))
+          ->where('filename', '=', pathinfo($catalog->img, PATHINFO_FILENAME))
+          ->where('extension', '=', pathinfo($catalog->img, PATHINFO_EXTENSION))
           ->first();
-          $catimg = isset($file['path'])?(Storage::disk('google')->exists($file['path'])?Storage::disk('google')->url($file['path']):NULL):NULL;
-        };
       };
+      $catimg = isset($catalog->img)?(isset($file['path'])?(Storage::disk('google')->exists($file['path'])?Storage::disk('google')->url($file['path']):NULL):NULL):NULL;
      //$catimg = dd(isset($categories->img)?(isset($file['path'])?(Storage::disk('google')->exists($file['path'])?Storage::disk('google')->url($file['path']):NULL):NULL):NULL);
       //
       return $view->with(['catalog' => $catalog, 'catimg' => $catimg]);
