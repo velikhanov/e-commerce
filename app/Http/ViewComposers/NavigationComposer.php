036835d604f9a985d0b6cb998a3a183d5496d893
velikhanov <teymurvk@mail.ru>
2021-04-26 20:30:43 +0400

    Update

--- app/Http/ViewComposers/NavigationComposer.php
+++ app/Http/ViewComposers/NavigationComposer.php
@@ -24,20 +24,20 @@ class NavigationComposer
             ->first();
         };
         $catimg = isset($cat->img)?(isset($file['path'])?(Storage::disk('google')->exists($file['path'])?Storage::disk('google')->url($file['path']):NULL):NULL):NULL;
-      };
-      foreach ($catalog->children as $subcat) {
-        if(isset($subcat->img)){
-            // $cat = Category::
-            // code...
-            $contents = collect(Storage::disk('google')->listContents('1lngtMrfEvcwjnJWp6b7Bxv2q5NDdYJze/', false));
-            $file = $contents
-            ->where('type', '=', 'file')
-            ->where('filename', '=', pathinfo($subcat->img, PATHINFO_FILENAME))
-            ->where('extension', '=', pathinfo($subcat->img, PATHINFO_EXTENSION))
-            ->first();
+
+        foreach ($cat->children as $subcat) {
+          if(isset($subcat->img)){
+              // code...
+              $contents = collect(Storage::disk('google')->listContents('1lngtMrfEvcwjnJWp6b7Bxv2q5NDdYJze/', false));
+              $file = $contents
+              ->where('type', '=', 'file')
+              ->where('filename', '=', pathinfo($subcat->img, PATHINFO_FILENAME))
+              ->where('extension', '=', pathinfo($subcat->img, PATHINFO_EXTENSION))
+              ->first();
+          };
+          $subcatimg = isset($subcat->img)?(isset($file['path'])?(Storage::disk('google')->exists($file['path'])?Storage::disk('google')->url($file['path']):NULL):NULL):NULL;
         };
-        $subcatimg = isset($subcat->img)?(isset($file['path'])?(Storage::disk('google')->exists($file['path'])?Storage::disk('google')->url($file['path']):NULL):NULL):NULL;
       };
-      return $view->with(['catalog' => $catalog, 'catimg' => $catimg]);
+      return $view->with(['catalog' => $catalog, 'catimg' => $catimg, 'subcatimg' => $subcatimg]);
   }
 }
