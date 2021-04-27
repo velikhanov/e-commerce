c467227968ab476713738cf485fc70750ae5c00a
velikhanov <teymurvk@mail.ru>
2021-04-27 16:57:49 +0400

    Update

--- app/Http/Controllers/BasketController.php
+++ app/Http/Controllers/BasketController.php
@@ -22,21 +22,21 @@ class BasketController extends Controller
     }
     $oldCart = Session::get('cart');
     $cart = new Cart($oldCart);
-    // $product = Product::get();
-    //   foreach($product as $prod){
-    //    $prodimg = null; //define it here as null
-    //     if(isset($prod->cardImage->path)){
-    //       $contents = collect(Storage::disk('google')->listContents('175IwF-UY0bKpii0UXnN7lKpv8nSZ9lmX/', false));
-    //       $file = $contents
-    //       ->where('type', '=', 'file')
-    //       ->where('filename', '=', pathinfo($prod->cardImage->path, PATHINFO_FILENAME))
-    //       ->where('extension', '=', pathinfo($prod->cardImage->path, PATHINFO_EXTENSION))
-    //       ->first();
-    //        $prodimg = isset($file['path'])?(Storage::disk('google')->exists($file['path'])?Storage::disk('google')->url($file['path']):NULL):NULL;
-    //     };
-    //   };
+    $product = Product::get();
+      foreach($product as $prod){
+       $prodimg = null; //define it here as null
+        if(isset($prod->cardImage->path)){
+          $contents = collect(Storage::disk('google')->listContents('175IwF-UY0bKpii0UXnN7lKpv8nSZ9lmX/', false));
+          $file = $contents
+          ->where('type', '=', 'file')
+          ->where('filename', '=', pathinfo($prod->cardImage->path, PATHINFO_FILENAME))
+          ->where('extension', '=', pathinfo($prod->cardImage->path, PATHINFO_EXTENSION))
+          ->first();
+           $prodimg = isset($file['path'])?(Storage::disk('google')->exists($file['path'])?Storage::disk('google')->url($file['path']):NULL):NULL;
+        };
+      };
 
-    return view('basket', ['products' => $cart->items, 'totalPrice' => $cart->totalPrice]);
+    return view('basket', ['products' => $cart->items, 'totalPrice' => $cart->totalPrice, 'prodimg' => $prodimg]);
   }
 
   public function addCart(Request $request, $id){
