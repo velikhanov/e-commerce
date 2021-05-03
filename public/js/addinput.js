032523285eb5a4cf07dfc29489ab714bb910ed00
velikhanov <teymurvk@mail.ru>
2021-05-03 04:00:22 +0400

    Update

--- public/js/addinput.js
+++ public/js/addinput.js
@@ -9,7 +9,7 @@ $("#addRow").click(function () {
         html += '<div id="inputFormRow">';
         html += '<div class="input-group mb-3">';
         html += '<input type="text" name="properties['+i+'][key]" class="key form-control m-input ml-3" placeholder="Key" autocomplete="off">';
-        html += '<input type="text" name="properties['+i+'][value]" class="valueform-control m-input ml-3" placeholder="Value" autocomplete="off">';
+        html += '<input type="text" name="properties['+i+'][value]" class="value form-control m-input ml-3" placeholder="Value" autocomplete="off">';
         html += '<div class="input-group-append ml-3">';
         html += '<button id="removeRow" type="button" class="btn btn-danger">Delete</button>';
         html += '</div>';
